efae3d70cef683c5573dc422b3677cc08d0ea1ed
Sergey Vasiliev <aragamerek@gmail.com>
2023-09-14 12:56:28 +0300

    feat: add context for postgre

---
+++ PsychoQuest/Repository/DbContext/PostgreDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Repository.DbContext;
+
+public class PostgreDbContext : Microsoft.EntityFrameworkCore.DbContext
+{
+    private readonly IConfiguration _configuration;
+
+    public PostgreDbContext(IConfiguration configuration)
+    {
+        _configuration = configuration;
+        Database.EnsureCreated();
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql(_configuration.GetSection("PostgreSql:ConnectionString").Value);
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<TestResults>()
+            .Property(e => e.TestName)
+            .HasConversion<string>();
+    }
+    
+    public DbSet<TestResults> TestResults { get; set; }
+}
