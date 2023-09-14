f3e322fd93e758accbe0f4841976c34201fa19d7
Sergey Vasiliev <aragamerek@gmail.com>
2023-09-14 12:59:10 +0300

    feat: add testAnswer repository

---
+++ PsychoQuest/Repository.Contracts/Repository/ITestAnswersRepository.cs
@@ -0,0 +1,15 @@
+﻿using Entities.Enums;
+using Entities.Models;
+
+namespace Repository.Contracts.Repository;
+
+public interface ITestAnswersRepository
+{
+    Task<TestAnswers> GetTestAnswersAsync(long userId, TypeTest typeTest);
+    
+    Task SaveTestAnswersAsync(TestAnswers testAnswers);
+    
+    Task DeleteTestAnswersAsync(long userId, TypeTest typeTest);
+    
+    Task<bool> TestAnswersExistsAsync(long userId, TypeTest typeTest);
+}
---
+++ PsychoQuest/Repository/Repository/TestAnswersRepository.cs
@@ -0,0 +1,46 @@
+﻿using Entities.Enums;
+using Entities.Models;
+using MongoDB.Driver;
+using Repository.Contracts.Repository;
+using Repository.DbContext;
+
+namespace Repository.Repository;
+
+public class TestAnswersRepository : ITestAnswersRepository
+{
+    private readonly MongoDbContext _mongoDbContext;
+
+    public TestAnswersRepository(MongoDbContext mongoDbContext) => _mongoDbContext = mongoDbContext;
+
+    public async Task<TestAnswers> GetTestAnswersAsync(long userId, TypeTest typeTest)
+    {
+        var filter = Builders<TestAnswers>.Filter.And(
+            Builders<TestAnswers>.Filter.Eq(nameof(TestAnswers.TestName), typeTest),
+                      Builders<TestAnswers>.Filter.Eq(nameof(TestAnswers.UserId), userId)
+        );
+        
+        var answers = await _mongoDbContext.TestAnswers.Find(filter).FirstOrDefaultAsync();// fix async
+        
+        return answers;
+    }
+
+    public async Task SaveTestAnswersAsync(TestAnswers testAnswers)
+    {
+       await _mongoDbContext.TestAnswers.InsertOneAsync(testAnswers);
+    }
+
+    public async Task DeleteTestAnswersAsync(long userId, TypeTest typeTest)
+    {
+        var filter = Builders<TestAnswers>.Filter.And(
+            Builders<TestAnswers>.Filter.Eq(nameof(TestAnswers.TestName), typeTest),
+                      Builders<TestAnswers>.Filter.Eq(nameof(TestAnswers.UserId), userId)
+        );
+
+        await _mongoDbContext.TestAnswers.DeleteOneAsync(filter);
+    }
+
+    public async Task<bool> TestAnswersExistsAsync(long userId, TypeTest typeTest)
+    {
+        return await GetTestAnswersAsync(userId, typeTest) is null;
+    }
+}
