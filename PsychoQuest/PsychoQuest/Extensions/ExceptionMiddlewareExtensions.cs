e26839fd0a1c65587967461a39616b02933727d8
Sergey Vasiliev <aragamerek@gmail.com>
2023-09-15 11:56:10 +0300

    feat: add middleware for exception

---
+++ PsychoQuest/PsychoQuest/Extensions/ExceptionMiddlewareExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System.Net;
+using Entities.ErrorModel;
+using Entities.Exceptions.NotFoundException;
+using Entities.Exceptions.OutOfRangeException;
+using Microsoft.AspNetCore.Diagnostics;
+
+namespace PsychoQuest.Extensions;
+
+public static class ExceptionMiddlewareExtensions
+{
+    public static void ConfigureExceptionHandler(this WebApplication app)
+    {
+        app.UseExceptionHandler(appError =>
+        {
+            appError.Run(async context =>
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                context.Response.ContentType = "application/json";
+                var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+                if (contextFeature != null)
+                {
+                    context.Response.StatusCode = contextFeature.Error switch
+                    {
+                        OutOfRangeException => StatusCodes.Status400BadRequest,
+                        NotFoundException => StatusCodes.Status404NotFound,
+                        _ => StatusCodes.Status500InternalServerError
+                    };
+                    await context.Response.WriteAsync(new ErrorDetails()
+                    {
+                        StatusCode = context.Response.StatusCode,
+                        Message = contextFeature.Error.Message,
+                    }.ToString());
+                }
+            });
+        });
+    }
+}
