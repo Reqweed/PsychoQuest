5b65d914d59f4eb44fe0651b141d0b906320d909
Sergey Vasiliev <aragamerek@gmail.com>
2023-09-15 11:49:18 +0300

    feat: add service manager

---
+++ PsychoQuest/Service.Contracts/IServiceManager.cs
@@ -0,0 +1,10 @@
+﻿using Service.Contracts.Services;
+
+namespace Service.Contracts;
+
+public interface IServiceManager
+{
+    IScaleBeckCalculationService ScaleBeck { get; }
+    
+    ITestHallCalculationService TestHall { get; }
+}
---
+++ PsychoQuest/Service/ServiceManager.cs
@@ -0,0 +1,21 @@
+﻿using Repository.Contracts;
+using Service.Contracts;
+using Service.Contracts.Services;
+using Service.Services;
+
+namespace Service;
+
+public class ServiceManager : IServiceManager
+{
+    private readonly Lazy<IScaleBeckCalculationService> _scaleBeckCalculationService;
+    private readonly Lazy<ITestHallCalculationService> _testHallCalculationService;
+    
+    public ServiceManager(IRepositoryManager repositoryManager)
+    {
+        _testHallCalculationService = new Lazy<ITestHallCalculationService>(() => new TestHallCalculationService(repositoryManager));
+        _scaleBeckCalculationService = new Lazy<IScaleBeckCalculationService>(() => new ScaleBeckCalculationService(repositoryManager));
+    }
+
+    public IScaleBeckCalculationService ScaleBeck => _scaleBeckCalculationService.Value;
+    public ITestHallCalculationService TestHall => _testHallCalculationService.Value;
+}
