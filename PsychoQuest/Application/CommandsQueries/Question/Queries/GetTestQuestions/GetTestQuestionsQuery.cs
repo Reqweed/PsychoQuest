ce0211ebd949c987c15a35d0dc4bb894a430aa74
Sergey Vasiliev <aragamerek@gmail.com>
2023-09-15 11:52:01 +0300

    feat: add mediatR for Questions

---
+++ PsychoQuest/Application/CommandsQueries/Question/Queries/GetTestQuestions/GetTestQuestionsQuery.cs
@@ -0,0 +1,10 @@
+﻿using Entities.Enums;
+using Entities.Models;
+using MediatR;
+
+namespace Application.CommandsQueries.Question.Queries.GetTestQuestions;
+
+public class GetTestQuestionsQuery : IRequest<TestQuestions>
+{
+    public TypeTest TypeTest { get; set; }
+}
---
+++ PsychoQuest/Application/CommandsQueries/Question/Queries/GetTestQuestions/GetTestQuestionsQueryHandler.cs
@@ -0,0 +1,23 @@
+﻿using Entities.Exceptions.NotFoundException;
+using Entities.Models;
+using MediatR;
+using Repository.Contracts;
+
+namespace Application.CommandsQueries.Question.Queries.GetTestQuestions;
+
+public class GetTestQuestionsQueryHandler : IRequestHandler<GetTestQuestionsQuery,TestQuestions>
+{
+    private readonly IRepositoryManager _repositoryManager;
+
+    public GetTestQuestionsQueryHandler(IRepositoryManager repositoryManager) => _repositoryManager = repositoryManager;
+
+    public async Task<TestQuestions> Handle(GetTestQuestionsQuery request, CancellationToken cancellationToken)
+    {
+        var questions = await _repositoryManager.TestQuestions.GetTestQuestionsAsync(request.TypeTest);
+
+        if (questions is null) 
+            throw new TestQuestionsNotFoundException(request.TypeTest);
+
+        return questions;
+    }
+}
