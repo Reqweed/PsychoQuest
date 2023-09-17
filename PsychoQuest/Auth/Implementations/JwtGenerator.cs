9ede23ad6f3a4d3ae8a480463e1a73aee062d8ee
Sergey Vasiliev <aragamerek@gmail.com>
2023-09-17 13:36:49 +0300

    feat: add JwtGenerator

---
+++ PsychoQuest/Auth/Implementations/JwtGenerator.cs
@@ -0,0 +1,88 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using Auth.Interfaces;
+using Entities.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Auth.Implementations;
+
+public class JwtGenerator : IJwtGenerator
+{
+    private readonly UserManager<User> _userManager;
+    private readonly IConfiguration _configuration;
+    private readonly SymmetricSecurityKey _key;
+
+    public JwtGenerator(UserManager<User> userManager, IConfiguration configuration)
+    {
+        _userManager = userManager;
+        _configuration = configuration;
+        _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Key"]));
+    }
+    
+    public string CreateToken(User user)
+    {
+        var claims = new List<Claim>()
+        {
+            new(JwtRegisteredClaimNames.NameId, user.Id.ToString()),
+            new(JwtRegisteredClaimNames.Gender, user.Gender.ToString()),
+            new(JwtRegisteredClaimNames.Email, user.Email!)
+        };
+        
+        var credentials = new SigningCredentials(_key,SecurityAlgorithms.HmacSha512Signature);
+
+        var tokenDescriptor = new SecurityTokenDescriptor()
+        {
+            Issuer = _configuration["JWT:Issuer"],
+            Audience = _configuration["JWT:Audience"],
+            Subject = new ClaimsIdentity(claims),
+            Expires = DateTime.Now.AddMinutes(5),
+            SigningCredentials = credentials
+        };
+        
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+
+        return tokenHandler.WriteToken(token);
+    }
+
+    public string CreateRefreshToken()
+    {
+        var randomNumber = new byte[32];
+
+        using var rng = RandomNumberGenerator.Create();
+        rng.GetBytes(randomNumber);
+
+        return Convert.ToBase64String(randomNumber);
+    }
+
+    public ClaimsPrincipal GetPrincipalFromExpiredToken(string token)
+    {
+        var tokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidIssuer = _configuration["JWT:Issuer"],
+            ValidAudience = _configuration["JWT:Audience"],
+            IssuerSigningKey = _key,
+            ValidateAudience = true,
+            ValidateIssuer = true,
+            ValidateIssuerSigningKey = true,
+            ValidateLifetime = false
+        };
+        
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var principal = tokenHandler.ValidateToken(token,
+            tokenValidationParameters, out var securityToken);
+        
+        var jwtSecurityToken = securityToken as JwtSecurityToken;
+        if (jwtSecurityToken == null || !jwtSecurityToken.Header.Alg.Equals(SecurityAlgorithms.HmacSha512,
+                StringComparison.InvariantCultureIgnoreCase))
+        {
+            throw new SecurityTokenException("Invalid token");
+        }
+
+        return principal;
+    }
+}
---
+++ PsychoQuest/Auth/Interfaces/IJwtGenerator.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Claims;
+using Entities.Models;
+
+namespace Auth.Interfaces;
+
+public interface IJwtGenerator
+{
+    string CreateToken(User user);
+    
+    string CreateRefreshToken();
+    
+    ClaimsPrincipal GetPrincipalFromExpiredToken(string token);
+}
