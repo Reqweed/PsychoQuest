e28dec23f7597c2e990124cc4eb912bfd35dfdc6
Sergey Vasiliev <aragamerek@gmail.com>
2023-09-18 10:11:46 +0300

    feat: add services and middleware

--- PsychoQuest/PsychoQuest/Program.cs
+++ PsychoQuest/PsychoQuest/Program.cs
@@ -1,10 +1,30 @@
+using Newtonsoft.Json.Converters;
+using PsychoQuest.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.ConfigureDbContext();
+builder.Services.ConfigureRepositoryManager();
+builder.Services.ConfigureServiceManager();
+builder.Services.ConfigureMediatR();
 
+builder.Services.AddAuth(builder.Configuration);
+
+builder.Services.AddControllers()
+    .AddApplicationPart(typeof(PsychoQuest.Presentation.AssemblyReference).Assembly)
+    .AddNewtonsoftJson(options =>
+    {
+        options.SerializerSettings.Converters.Add(new StringEnumConverter());
+    });
+
+builder.Services.ConfigureRouteOptions();
 
 var app = builder.Build();
 
+app.ConfigureExceptionHandler();
 
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers();
 
