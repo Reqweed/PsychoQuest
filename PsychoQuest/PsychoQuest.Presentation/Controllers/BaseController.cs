2785ee08171f7518c820d06543b9e349a99cee36
Sergey Vasiliev <aragamerek@gmail.com>
2023-09-18 09:54:07 +0300

    feat: add base controller

---
+++ PsychoQuest/PsychoQuest.Presentation/Controllers/BaseController.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Claims;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PsychoQuest.Presentation.Controllers;
+
+[ApiController]
+public abstract class BaseController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    protected BaseController(IMediator mediator) => _mediator = mediator;
+
+    protected IMediator Mediator => _mediator;
+
+    protected long UserId => User.Identity!.IsAuthenticated ? long.Parse(User.FindFirst(ClaimTypes.NameIdentifier)?.Value) : 0;
+}
