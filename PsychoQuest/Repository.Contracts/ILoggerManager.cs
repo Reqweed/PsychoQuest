e87dd1050303adb3657ea8bf770c2843b99b4d3b
Sergey Vasiliev <aragamerek@gmail.com>
2023-10-10 13:33:22 +0300

    feat: add nlog

---
+++ PsychoQuest/Repository.Contracts/ILoggerManager.cs
@@ -0,0 +1,12 @@
+namespace Repository.Contracts;
+
+public interface ILoggerManager
+{
+    void LogDebug(string message);
+    
+    void LogError(string message);
+    
+    void LogInfo(string message);
+    
+    void LogWarn(string message);
+}
