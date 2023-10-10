014edfa0c4168267864e11ca820c5df483067ad2
Sergey Vasiliev <aragamerek@gmail.com>
2023-10-10 15:16:50 +0300

    feat: add logging to questions mediatr

--- PsychoQuest/Application/CommandsQueries/Question/Queries/GetTestQuestions/GetTestQuestionsQueryHandler.cs
+++ PsychoQuest/Application/CommandsQueries/Question/Queries/GetTestQuestions/GetTestQuestionsQueryHandler.cs
@@ -8,15 +8,28 @@ namespace Application.CommandsQueries.Question.Queries.GetTestQuestions;
 public class GetTestQuestionsQueryHandler : IRequestHandler<GetTestQuestionsQuery,TestQuestions>
 {
     private readonly IRepositoryManager _repositoryManager;
+    private readonly ILoggerManager _loggerManager;
 
-    public GetTestQuestionsQueryHandler(IRepositoryManager repositoryManager) => _repositoryManager = repositoryManager;
+    public GetTestQuestionsQueryHandler(IRepositoryManager repositoryManager, ILoggerManager loggerManager)
+    {
+        _repositoryManager = repositoryManager;
+        _loggerManager = loggerManager;
+    }
 
     public async Task<TestQuestions> Handle(GetTestQuestionsQuery request, CancellationToken cancellationToken)
     {
+        _loggerManager.LogInfo($"Query:GetTestQuestionsQuery - Questions:{request.TypeTest} has begun");
+
         var questions = await _repositoryManager.TestQuestions.GetTestQuestionsAsync(request.TypeTest);
 
-        if (questions is null) 
+        if (questions is null)
+        {
+            _loggerManager.LogWarn($"Query:GetTestQuestionsQuery - Questions:{request.TypeTest} doesn't exist");
+
             throw new TestQuestionsNotFoundException(request.TypeTest);
+        }
+
+        _loggerManager.LogInfo($"Query:GetTestQuestionsQuery - Questions:{request.TypeTest} was finished");
 
         return questions;
     }
