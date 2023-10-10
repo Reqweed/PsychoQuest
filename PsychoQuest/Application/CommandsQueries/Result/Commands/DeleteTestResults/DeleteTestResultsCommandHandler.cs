ebde33668428e289e1cb6d00417a3596a6438c7c
Sergey Vasiliev <aragamerek@gmail.com>
2023-10-10 15:13:02 +0300

    feat: add logging to test results mediatr

--- PsychoQuest/Application/CommandsQueries/Result/Commands/DeleteTestResults/DeleteTestResultsCommandHandler.cs
+++ PsychoQuest/Application/CommandsQueries/Result/Commands/DeleteTestResults/DeleteTestResultsCommandHandler.cs
@@ -7,14 +7,27 @@ namespace Application.CommandsQueries.Result.Commands.DeleteTestResults;
 public class DeleteTestResultsCommandHandler : IRequestHandler<DeleteTestResultsCommand>
 {
     private readonly IRepositoryManager _repositoryManager;
+    private readonly ILoggerManager _loggerManager;
 
-    public DeleteTestResultsCommandHandler(IRepositoryManager repositoryManager) => _repositoryManager = repositoryManager;
+    public DeleteTestResultsCommandHandler(IRepositoryManager repositoryManager, ILoggerManager loggerManager)
+    {
+        _repositoryManager = repositoryManager;
+        _loggerManager = loggerManager;
+    }
 
     public async Task Handle(DeleteTestResultsCommand request, CancellationToken cancellationToken)
     {
+        _loggerManager.LogInfo($"Command:DeleteTestResults - User with id:{request.UserId} and Test:{request.TypeTest} has begun");
+
         if (await _repositoryManager.TestResults.TestResultExistsAsync(request.UserId, request.TypeTest))
+        {
+            _loggerManager.LogWarn($"Command:DeleteTestResults - User with id:{request.UserId} and Test:{request.TypeTest} doesn't exist");
+   
             throw new TestResultsNotFoundException(request.UserId, request.TypeTest);
+        }
         
         await _repositoryManager.TestResults.DeleteTestResultsAsync(request.UserId, request.TypeTest);
+        
+        _loggerManager.LogInfo($"Command:DeleteTestResults - User with id:{request.UserId} and Test:{request.TypeTest} was finished");
     }
 }
--- PsychoQuest/Application/CommandsQueries/Result/Queries/GetTestResults/GetTestResultsQueryHandler.cs
+++ PsychoQuest/Application/CommandsQueries/Result/Queries/GetTestResults/GetTestResultsQueryHandler.cs
@@ -8,15 +8,28 @@ namespace Application.CommandsQueries.Result.Queries.GetTestResults;
 public class GetTestResultsQueryHandler : IRequestHandler<GetTestResultsQuery,TestResults>
 {
     private readonly IRepositoryManager _repositoryManager;
+    private readonly ILoggerManager _loggerManager;
 
-    public GetTestResultsQueryHandler(IRepositoryManager repositoryManager) => _repositoryManager = repositoryManager;
+    public GetTestResultsQueryHandler(IRepositoryManager repositoryManager, ILoggerManager loggerManager)
+    {
+        _repositoryManager = repositoryManager;
+        _loggerManager = loggerManager;
+    }
 
     public async Task<TestResults> Handle(GetTestResultsQuery request, CancellationToken cancellationToken)
     {
-        if(await _repositoryManager.TestResults.TestResultExistsAsync(request.UserId, request.TypeTest)) 
+        _loggerManager.LogInfo($"Query:GetTestResultsQuery - User with id:{request.UserId} and Test:{request.TypeTest} has begun");
+        
+        if (await _repositoryManager.TestResults.TestResultExistsAsync(request.UserId, request.TypeTest))
+        {
+            _loggerManager.LogWarn($"Query:GetTestResultsQuery - User hadn't test results:{request.TypeTest} doesn't exist");
+            
             throw new TestResultsNotFoundException(request.UserId, request.TypeTest);
-
+        }
+        
         var results = await _repositoryManager.TestResults.GetTestResultsAsync(request.UserId, request.TypeTest);
+
+        _loggerManager.LogInfo($"Query:GetTestResultsQuery - User with id:{request.UserId} and Test:{request.TypeTest} was finished");
 
         return results;
     }
