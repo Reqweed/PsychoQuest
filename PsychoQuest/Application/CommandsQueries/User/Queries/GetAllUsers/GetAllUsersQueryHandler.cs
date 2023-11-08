d3ab8e90979aa2bded1b7e9e537e9aba905511c0
Sergey Vasiliev <aragamerek@gmail.com>
2023-11-08 12:51:50 +0300

    feat: add caching to user mediatr

--- PsychoQuest/Application/CommandsQueries/User/Queries/GetAllUsers/GetAllUsersQueryHandler.cs
+++ PsychoQuest/Application/CommandsQueries/User/Queries/GetAllUsers/GetAllUsersQueryHandler.cs
@@ -1,4 +1,6 @@
-﻿using MediatR;
+﻿using Application.Caches.Implementations;
+using Application.Caches.Interfaces;
+using MediatR;
 using Repository.Contracts;
 
 namespace Application.CommandsQueries.User.Queries.GetAllUsers;
@@ -7,19 +9,24 @@ public class GetAllUsersQueryHandler : IRequestHandler<GetAllUsersQuery,IEnumera
 {
     private readonly IRepositoryManager _repositoryManager;
     private readonly ILoggerManager _loggerManager;
+    private readonly ICacheManager<IEnumerable<Entities.Models.User>> _cacheManager;
 
-    public GetAllUsersQueryHandler(IRepositoryManager repositoryManager, ILoggerManager loggerManager)
+    public GetAllUsersQueryHandler(IRepositoryManager repositoryManager, ILoggerManager loggerManager, ICacheManager<IEnumerable<Entities.Models.User>> cacheManager)
     {
         _repositoryManager = repositoryManager;
         _loggerManager = loggerManager;
+        _cacheManager = cacheManager;
     }
 
     public async Task<IEnumerable<Entities.Models.User>> Handle(GetAllUsersQuery request, CancellationToken cancellationToken)
     {
         _loggerManager.LogInfo($"Query:GetAllUsersQuery has begun");
         
-        var users = await _repositoryManager.User.GetAllUsers(cancellationToken);
+        var usersFunc = async() => await _repositoryManager.User.GetAllUsers(cancellationToken);
 
+        _cacheManager.CacheEntryOptions = CacheEntryOption.DefaultCacheEntry;
+        var users = await _cacheManager.GetOrSetCacheValue("All-users", usersFunc);
+            
         _loggerManager.LogInfo($"Query:GetAllUsersQuery was finished");
         
         return users;
--- PsychoQuest/Application/CommandsQueries/User/Queries/GetUser/GetUserQueryHandler.cs
+++ PsychoQuest/Application/CommandsQueries/User/Queries/GetUser/GetUserQueryHandler.cs
@@ -1,4 +1,6 @@
-﻿using Entities.Exceptions.NotFoundException;
+﻿using Application.Caches.Implementations;
+using Application.Caches.Interfaces;
+using Entities.Exceptions.NotFoundException;
 using MediatR;
 using Repository.Contracts;
 
@@ -8,18 +10,23 @@ public class GetUserQueryHandler : IRequestHandler<GetUserQuery,Entities.Models.
 {
     private readonly IRepositoryManager _repositoryManager;
     private readonly ILoggerManager _loggerManager;
+    private readonly ICacheManager<Entities.Models.User> _cacheManager;
 
-    public GetUserQueryHandler(IRepositoryManager repositoryManager, ILoggerManager loggerManager)
+    public GetUserQueryHandler(IRepositoryManager repositoryManager, ILoggerManager loggerManager, ICacheManager<Entities.Models.User> cacheManager)
     {
         _repositoryManager = repositoryManager;
         _loggerManager = loggerManager;
+        _cacheManager = cacheManager;
     }
 
     public async Task<Entities.Models.User> Handle(GetUserQuery request, CancellationToken cancellationToken)
     {
         _loggerManager.LogInfo($"Query:GetUserQuery - User with id:{request.UserId} has begun");
 
-        var user = await _repositoryManager.User.GetUserAsync(request.UserId, cancellationToken);
+        var userFunc = async() => await _repositoryManager.User.GetUserAsync(request.UserId, cancellationToken);
+
+        _cacheManager.CacheEntryOptions = CacheEntryOption.DefaultCacheEntry;
+        var user = await _cacheManager.GetOrSetCacheValue("user", userFunc);
         if (user is null)
         {
             _loggerManager.LogWarn($"Query:GetUserQuery - User with id:{request.UserId} doesn't exist");
